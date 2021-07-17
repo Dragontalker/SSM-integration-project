17423c4ace426f6b4eee2f6033eae0d89c3d1d85
Richard <richard.yang.tong@gmail.com>
2021-07-17 13:25:34 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/test/MvcTest.java
+++ ssm-crud/src/main/java/com/dragontalker/test/MvcTest.java
@@ -1,13 +1,29 @@
 package com.dragontalker.test;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration(locations = {
 		"classpath:applicationContext.xml", 
 		"file:src/main/webapp/WEB-INF/dispatcherServlet-servlet.xml"})
 public class MvcTest {
+	
+	@Autowired
+	WebApplicationContext context;
 
+	MockMvc mockMvc;
+	
+	@Before
+	public void initMockMvc() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
 }
