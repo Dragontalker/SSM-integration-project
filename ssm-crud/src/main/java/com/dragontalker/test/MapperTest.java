60375074ed61256fca1db279b4e34308ec3203f3
Richard <richard.yang.tong@gmail.com>
2021-07-17 11:47:48 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/test/MapperTest.java
+++ ssm-crud/src/main/java/com/dragontalker/test/MapperTest.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.dragontalker.bean.Department;
-import com.dragontalker.bean.Employee;
 import com.dragontalker.dao.DepartmentMapper;
 import com.dragontalker.dao.EmployeeMapper;
 
