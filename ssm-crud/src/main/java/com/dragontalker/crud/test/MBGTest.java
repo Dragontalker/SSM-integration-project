791cef23e1ea45dfeeb94daa032f90b0d5558170
Richard <richard.yang.tong@gmail.com>
2021-07-17 09:25:21 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/crud/test/MBGTest.java
+++ ssm-crud/src/main/java/com/dragontalker/crud/test/MBGTest.java
@@ -4,11 +4,17 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+
 public class MBGTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		File configfile = new File("generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configfile);
+		
 	}
 }
