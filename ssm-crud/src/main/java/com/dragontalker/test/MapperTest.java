c74f423cce873f9448722f617227dfe329c86d29
Richard <richard.yang.tong@gmail.com>
2021-07-17 11:23:47 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/test/MapperTest.java
+++ ssm-crud/src/main/java/com/dragontalker/test/MapperTest.java
@@ -1,11 +1,25 @@
 package com.dragontalker.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.dragontalker.dao.DepartmentMapper;
+
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class MapperTest {
 
+	@Autowired
+	DepartmentMapper departmentMapper;
+	
+	@Test
+	public void testCRUD() {
+	
+		System.out.println(departmentMapper);
+	}
 }
