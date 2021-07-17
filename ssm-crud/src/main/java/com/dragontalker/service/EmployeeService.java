aae59f6b2f48505d667f006401c2e6d0340197f4
Richard <richard.yang.tong@gmail.com>
2021-07-17 13:09:53 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/service/EmployeeService.java
+++ ssm-crud/src/main/java/com/dragontalker/service/EmployeeService.java
@@ -1,5 +1,14 @@
 package com.dragontalker.service;
 
+import java.util.List;
+
+import com.dragontalker.bean.Employee;
+
 public class EmployeeService {
+
+	public List<Employee> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
