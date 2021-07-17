127be16e7e67f7a64523cbcba814d82b5abd85a8
Richard <richard.yang.tong@gmail.com>
2021-07-17 09:23:22 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/crud/test/MBGTest.java
+++ ssm-crud/src/main/java/com/dragontalker/crud/test/MBGTest.java
@@ -1,6 +1,11 @@
 package com.dragontalker.crud.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MBGTest {
 
-	
+	public static void main(String[] args) {
+		List<String> warnings = new ArrayList<String>();
+	}
 }
