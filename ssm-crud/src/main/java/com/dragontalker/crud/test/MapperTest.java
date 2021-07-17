612cd78cb677caa000991864b635ecea4e232285
Richard <richard.yang.tong@gmail.com>
2021-07-17 10:42:44 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/crud/test/MapperTest.java
+++ ssm-crud/src/main/java/com/dragontalker/crud/test/MapperTest.java
@@ -3,10 +3,11 @@ package com.dragontalker.crud.test;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
 import com.dragontalker.crud.dao.DepartmentMapper;
 
-
+@ContextConfiguration
 public class MapperTest {
 
 	/**
