d7a7798210bf36026ccda0be12e9e9412155a92a
Richard <richard.yang.tong@gmail.com>
2021-07-17 13:09:38 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/controller/EmployeeController.java
+++ ssm-crud/src/main/java/com/dragontalker/controller/EmployeeController.java
@@ -1,13 +1,24 @@
 package com.dragontalker.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dragontalker.bean.Employee;
+import com.dragontalker.service.EmployeeService;
+
 @Controller
 public class EmployeeController {
+	
+	@Autowired
+	EmployeeService employeeService;
 
 	@RequestMapping("/emps")
 	public String getEmps() {
+		
+		List<Employee> emps = employeeService.getAll();
 		return "list";
 	}
 }
