8385afeb1372c40e64bed9fc10462f73a6848c2f
Richard <richard.yang.tong@gmail.com>
2021-07-17 13:23:19 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/test/MvcTest.java
+++ ssm-crud/src/main/java/com/dragontalker/test/MvcTest.java
@@ -5,7 +5,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+@ContextConfiguration(locations = {
+		"classpath:applicationContext.xml", 
+		"file:src/main/webapp/WEB-INF/dispatcherServlet-servlet.xml"})
 public class MvcTest {
 
 }
