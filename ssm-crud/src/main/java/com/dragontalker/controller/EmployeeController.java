50c1f9f3d1079e28e8835c60530a91e88d720c0a
Richard <richard.yang.tong@gmail.com>
2021-07-17 13:03:45 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/controller/EmployeeController.java
+++ ssm-crud/src/main/java/com/dragontalker/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.dragontalker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EmployeeController {
 
+	@RequestMapping("/emps")
 	public String getEmps() {
 		return "list";
 	}
