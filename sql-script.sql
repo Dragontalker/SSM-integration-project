5b4858511288dc73560999a623bc2e073e62a7da
Richard <richard.yang.tong@gmail.com>
2021-07-16 22:20:54 -0400

    Added notes.

--- sql-script.sql
+++ sql-script.sql
@@ -1,3 +1,11 @@
 DROP DATABASE IF EXISTS ssm_crud;
 CREATE DATABASE ssm_crud;
 USE ssm_crud;
+
+DROP TABLE IF EXISTS tbl_emp;
+CREATE TABLE tbl_emp(
+	`emp_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `emp_name` VARCHAR(255) NOT NULL,
+    `gender` CHAR(1),
+    `email` VARCHAR(255)
+);
