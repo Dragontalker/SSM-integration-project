007715b08e75f852d4ccb5db0ec60f9f95e83b62
Richard <richard.yang.tong@gmail.com>
2021-07-17 13:19:25 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/controller/EmployeeController.java
+++ ssm-crud/src/main/java/com/dragontalker/controller/EmployeeController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dragontalker.bean.Employee;
 import com.dragontalker.service.EmployeeService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 @Controller
 public class EmployeeController {
@@ -17,9 +21,18 @@ public class EmployeeController {
 	EmployeeService employeeService;
 
 	@RequestMapping("/emps")
-	public String getEmps(@RequestParam(value="pn", defaultValue="1")Integer pn) {
+	public String getEmps(
+			@RequestParam(value="pn", defaultValue="1")Integer pn,
+			@ModelAttribute Model model) {
+		
+		PageHelper.startPage(pn, 5);
 		
 		List<Employee> emps = employeeService.getAll();
+		
+		PageInfo page = new PageInfo(emps);
+		
+		model.addAttribute("pageInfo", page);
+		
 		return "list";
 	}
 }
