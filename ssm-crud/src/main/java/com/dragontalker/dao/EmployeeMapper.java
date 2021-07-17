87e1437d5494a1abab10256d85d963187541e27e
Richard <richard.yang.tong@gmail.com>
2021-07-17 11:29:01 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/dao/EmployeeMapper.java
+++ ssm-crud/src/main/java/com/dragontalker/dao/EmployeeMapper.java
@@ -19,6 +19,10 @@ public interface EmployeeMapper {
     List<Employee> selectByExample(EmployeeExample example);
 
     Employee selectByPrimaryKey(Integer empId);
+    
+    List<Employee> selectByExampleWithDept(EmployeeExample example);
+
+    Employee selectByPrimaryKeyWithDept(Integer empId);
 
     int updateByExampleSelective(@Param("record") Employee record, @Param("example") EmployeeExample example);
 
