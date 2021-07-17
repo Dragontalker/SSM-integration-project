e4cc230e5d3d621089366099129ae4b64b262351
Richard <richard.yang.tong@gmail.com>
2021-07-17 13:32:47 -0400

    Added notes.

--- ssm-crud/src/main/java/com/dragontalker/test/MvcTest.java
+++ ssm-crud/src/main/java/com/dragontalker/test/MvcTest.java
@@ -3,12 +3,17 @@ package com.dragontalker.test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.github.pagehelper.PageInfo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -25,5 +30,24 @@ public class MvcTest {
 	@Before
 	public void initMockMvc() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+	
+	public void testPage() throws Exception{
+		MvcResult result = mockMvc.perform(
+				MockMvcRequestBuilders
+					.get("/emps")
+					.param("pn", "1"))
+					.andReturn();
+		
+		MockHttpServletRequest request =  result.getRequest();
+		PageInfo pi = (PageInfo) request.getAttribute("pageInfo");
+		System.out.println(">> Current page No.: " + pi.getPageNum());
+		System.out.println(">> Total number of pages: " + pi.getPages());
+		System.out.println(">> Total number of records: " + pi.getTotal());
+		System.out.println(">> Page numbers needed to be displayed: ");
+		int[] nums = pi.getNavigatepageNums();
+		for (int i : nums) {
+			System.out.print(" " + i);
+		}
 	}
 }
